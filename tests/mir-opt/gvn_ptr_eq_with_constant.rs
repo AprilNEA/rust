d1b163cdbc1e5251ee2890f6ecc8c8d883b5305e
AprilNEA <github@sku.moe>
2025-12-29 16:18:18 +0800

    Add FileCheck annotations to gvn_ptr_eq_with_constant mir-opt test
    Part of #116971.

--- tests/mir-opt/gvn_ptr_eq_with_constant.rs
+++ tests/mir-opt/gvn_ptr_eq_with_constant.rs
@@ -1,4 +1,3 @@
-// skip-filecheck
 //@ test-mir-pass: GVN
 //@ only-64bit
 //@ compile-flags: -Z mir-enable-passes=+Inline
@@ -17,5 +16,11 @@ impl<T> Foo<T> {
 
 // EMIT_MIR gvn_ptr_eq_with_constant.main.GVN.diff
 pub fn main() {
+    // CHECK-LABEL: fn main(
+    // CHECK: (inlined Foo::<u8>::cmp_ptr)
+    // CHECK: [[ptr:_.*]] = const {0x1 as *mut u8};
+    // CHECK: [[a:_.*]] = const {0x1 as *const u8};
+    // CHECK: [[b:_.*]] = const {0x1 as *const u8};
+    // CHECK: {{_.*}} = const true;
     Foo::<u8>::cmp_ptr(std::ptr::dangling_mut());
 }
